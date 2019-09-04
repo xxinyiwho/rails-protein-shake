74bfc434ddf613671d554953c4aec15fbdee786f
Xinyi Wu <xxinyiwho@gmail.com>
2019-09-04 23:01:15 +0200

    FIX: the navbar style is fine

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "shakes#index"
+  root to: "pages#home"
   resources :shakes do
     resources :doses, only: [:new, :create]
   end
